d0df1c5a67f4d2c6bd18ea6908a97d6de65b211e
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-01 23:11:20 +0530

    middlewares 1

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const { pathname } = req.nextUrl;
+
+        // allow related routes
+      },
+    },
+  }
+);
